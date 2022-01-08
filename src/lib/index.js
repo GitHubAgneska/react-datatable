fef0877d7e898c0d73ed9d2fe59e5de89c170cb4
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2022-01-08 10:39:52 +0100

    correction missing index.js imports/exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,10 +1,29 @@
-import Button from './Button'
+import Button from './Button/Button'
 import Pagination from './Pagination/Pagination'
 import SearchBox from './SearchBox/SearchBox'
 import SelectEntriesBox from './SelectEntriesBox/SelectEntriesBox'
 import Table from './Table/Table'
 import Datatable from './Datatable'
-import DataTable_style from './DataTable_style'
+import { 
+    ComponentWrapper,
+    TableWrapper,
+    StyledTable,
+    StyledTableHeader,
+    TableHeaderIconWrapper,
+    StyledTableRow
+} from './DataTable_style'
 
 
-export { Button, Pagination, SearchBox, SelectEntriesBox, Table, Datatable,  DataTable_style }
+export { 
+    Button,
+    Pagination,
+    SearchBox,
+    SelectEntriesBox,
+    Table,
+    Datatable,
+    ComponentWrapper,
+    TableWrapper,
+    StyledTable,
+    StyledTableHeader,
+    TableHeaderIconWrapper,
+    StyledTableRow }
